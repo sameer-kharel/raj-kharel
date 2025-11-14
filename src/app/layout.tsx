7929a108927f52e1f2f27dd561dc27e62cbc8c77
sameer-kharel <shameerkharel@gmail.com>
2025-11-15 00:47:04 +0545

    made footer glitch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-900 text-white`}>
-        <Header />
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import About from './about/page';
 import Properties from './properties/page';
 import ListingsPage from './listings/page';
 import Contact from './contact/page'
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Properties />
       <ListingsPage />
       <Contact />
+      <Footer />
     </main>
   );
 }
